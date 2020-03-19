d41cf3d4daa5197739368a037b221101db5125f7
biancardona <biancardona.rivera@gmail.com>
2020-03-18 21:24:53 -0600

    adjusting changes in view navbar, modifying template string to display sign up, login, although it is not finished

--- src/app.js
+++ src/app.js
@@ -1,49 +1,50 @@
-
+"use strict";
+import Utils from './utils.js';
 
 import Home from './views/pages/home.js';
 
-// import PostShow from './views/pages/PostShow.js';
+//import PostShow from './views/pages/postShow.js';
 import Register from './views/pages/register.js';
 
 import Navbar from './views/pages/navbar.js';
-// import Bottombar from './views/components/Bottombar.js';
-
-// import Utils from './services/Utils.js';
+//import Bottombar from './views/components/Bottombar.js';
 
 // List of supported routes. Any url other than these routes will throw a 404 error
 const routes = {
-  '/': Home,
-  '/p/:id': PostShow,
-  '/register': Register,
+  '/'             : Home
+//  , '/p/:id'      : PostShow
+  , '/register'   : Register
 };
 
 
 // The router code. Takes a URL, checks against the list of supported routes and then renders the corresponding content page.
 const router = async () => {
+
   // Lazy load view element:
   const header = null || document.getElementById('header_container');
   const content = null || document.getElementById('page_container');
   const footer = null || document.getElementById('footer_container');
-
+  
   // Render the Header and footer of the page
   header.innerHTML = await Navbar.render();
   await Navbar.after_render();
-  footer.innerHTML = await Bottombar.render();
-  await Bottombar.after_render();
+ // footer.innerHTML = await Bottombar.render();
+ // await Bottombar.after_render();
 
 
   // Get the parsed URl from the addressbar
-  const request = Utils.parseRequestURL();
+  let request = Utils.parseRequestURL()
 
   // Parse the URL and if it has an id part, change it with the string ":id"
-  const parsedURL = (request.resource ? `/${request.resource}` : '/') + (request.id ? '/:id' : '') + (request.verb ? `/${request.verb}` : '');
-
+  let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
+  
   // Get the page from our hash of supported routes.
   // If the parsed URL is not in our list of supported routes, select the 404 page instead
-  const page = routes[parsedURL] ? routes[parsedURL] : Error404;
+  let page = routes[parsedURL] ? routes[parsedURL] : Error404
   content.innerHTML = await page.render();
   await page.after_render();
-};
+
+}
 
 // Listen on hash change:
 window.addEventListener('hashchange', router);
