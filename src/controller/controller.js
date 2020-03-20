113b92d59a2413ad831012c45ed02cde4c6589a3
biancardona <biancardona.rivera@gmail.com>
2020-03-20 15:38:26 -0600

    the user is logged into the firebase data

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,6 +3,16 @@ import Utils from '../../utils/utils.js';
 
 
 export const createUser = (email, password) => {
-    validatedEmail = Utils.validateEmail(email);
-    console.log(validatedEmail);
+    if (Utils.validateEmail(email) === true){
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+          });
+
+    }
+    
+
 }
--- src/model.js
+++ src/model.js
@@ -1,6 +1,5 @@
-
-
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+/*
+ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
